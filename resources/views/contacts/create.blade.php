1ab2c1fbba125eb5358998a7084afe2a5f9d0a39
Marvin Santiago <marvin.santiago@ymail.com>
2015-10-14 19:13:11 -0500

    Created partials for errors and form

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -4,35 +4,9 @@
 <h2>Add a New Contact</h2>
 <hr>
 {!! Form::open(['url' => 'contacts']) !!}
-
-<div class="form-group">
-    {!! Form::label('name', 'Name:', ['class' => 'control-label']) !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('address', 'Address:', ['class' => 'control-label']) !!}
-    {!! Form::text('address', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('phone', 'Phone:', ['class' => 'control-label']) !!}
-    {!! Form::text('phone', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('email', 'Email:', ['class' => 'control-label']) !!}
-    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-</div>
-
-
-{!! Form::submit('Create New Contact', ['class' => 'btn btn-primary']) !!}
+    @include ('contacts.form', ['submitButton' => 'Add New Contact'])
 
 {!! Form::close() !!}
 
-    @if ($errors->any())
-        <ul class="alert alert-danger">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+@include ('errors.list')
 @stop
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -5,35 +5,9 @@
 
 <hr>
 {!! Form::model($contact, ['method' => 'PATCH', 'action' => ['ContactsController@update', $contact->id]]) !!}
-
-<div class="form-group">
-    {!! Form::label('name', 'Name:', ['class' => 'control-label']) !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('address', 'Address:', ['class' => 'control-label']) !!}
-    {!! Form::text('address', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('phone', 'Phone:', ['class' => 'control-label']) !!}
-    {!! Form::text('phone', null, ['class' => 'form-control']) !!}
-</div>
-<div class="form-group">
-    {!! Form::label('email', 'Email:', ['class' => 'control-label']) !!}
-    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-</div>
-
-
-{!! Form::submit('Create New Contact', ['class' => 'btn btn-primary']) !!}
-
+    @include ('contacts.form', ['submitButton' => 'Update Info'])
 {!! Form::close() !!}
 
-    @if ($errors->any())
-        <ul class="alert alert-danger">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+@include ('errors.list')
+
 @stop
